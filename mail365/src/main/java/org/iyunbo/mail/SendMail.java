d8f465ed08d0e9a77c559a0cc12ba94cf148a828
iyunbo <yunbo.wang@solven.eu>
2019-07-07 17:00:39 +0200

    clean: final var

--- mail365/src/main/java/org/iyunbo/mail/SendMail.java
+++ mail365/src/main/java/org/iyunbo/mail/SendMail.java
@@ -9,16 +9,16 @@ class SendMail {
 
 
 	void execute() throws MessagingException {
-		Properties props = new Properties();
+		final Properties props = new Properties();
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.connectiontimeout", "3600");
 		props.put("mail.smtp.timeout", "3600");
 		props.put("mail.debug", true);
-		Session session = Session.getInstance(props);
+		final Session session = Session.getInstance(props);
 		session.setDebug(true);
 
-		Transport transport = session.getTransport();
+		final Transport transport = session.getTransport();
 		transport.connect("smtp.office365.com", 587, "yunbo.wang@outlook.com", "zedfretg0607");
 	}
 }
